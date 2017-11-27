005deb477b50344246257b4c4bd8aef3a8be7e3a
Gokce Yucel <gkcycl@gmail.com>
2017-11-28 01:05:03 +0300

    Get user by username

--- src/main/java/com/zenith/livinghistory/api/zenithlivinghistoryapi/controller/UserController.java
+++ src/main/java/com/zenith/livinghistory/api/zenithlivinghistoryapi/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.zenith.livinghistory.api.zenithlivinghistoryapi.controller;
 
 import com.zenith.livinghistory.api.zenithlivinghistoryapi.data.repository.ContentRepository;
+import com.zenith.livinghistory.api.zenithlivinghistoryapi.data.repository.UserRepository;
 import com.zenith.livinghistory.api.zenithlivinghistoryapi.dto.Content;
+import com.zenith.livinghistory.api.zenithlivinghistoryapi.dto.User;
 import com.zenith.livinghistory.api.zenithlivinghistoryapi.dto.request.ContentsByCreator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,18 +15,25 @@ import java.util.List;
 @RequestMapping("api/v1/users")
 public class UserController {
     private ContentRepository contentRepository;
+    private UserRepository userRepository;
 
-    public UserController(ContentRepository contentRepository) {
+    public UserController(ContentRepository contentRepository, UserRepository userRepository) {
         this.contentRepository = contentRepository;
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{username}")
+    public User get(@PathVariable String username) {
+        return userRepository.findFirstByUsername(username);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{username}/contents")
-    public List<Content> get(@PathVariable String username) {
+    public List<Content> getContents(@PathVariable String username) {
         return contentRepository.findContentsByUsername(username);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/contents")
-    public List<Content> get(@RequestBody ContentsByCreator request) {
+    public List<Content> getContents(@RequestBody ContentsByCreator request) {
         return contentRepository.findContentsByCreator(request.getCreator());
     }
 }
