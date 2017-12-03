a4a9b7e870510f909ce60734670a175a09e4442b
Brk <kazancibulent@gmail.com>
2017-12-03 20:39:01 +0300

    implemented end point to get properties of semantic annotation.

--- src/main/java/com/zenith/livinghistory/api/zenithlivinghistoryapi/common/SparQL/Queries.java
+++ src/main/java/com/zenith/livinghistory/api/zenithlivinghistoryapi/common/SparQL/Queries.java
@@ -3,36 +3,6 @@ package com.zenith.livinghistory.api.zenithlivinghistoryapi.common.SparQL;
 public final class Queries {
 
     /**
-     * @param - Full name of person.
-     */
-    public static String person =
-              "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n"
-            + "PREFIX  dbo: <http://dbpedia.org/ontology/>\n"
-            + "SELECT DISTINCT ?person ?name\n"
-            + "WHERE {\n"
-            + "?person a foaf:Person; \n"
-            + "a dbo:Person; \n"
-            + "foaf:name ?name. \n"
-            + "FILTER langMatches(lang(?name),\"en\") \n"
-            + "FILTER(CONTAINS(?name, \"%1$s\")) \n"
-            + "}";
-
-    /**
-     * @param - Name of the city.
-     */
-    public static  String place =
-              "PREFIX  dbo: <http://dbpedia.org/ontology/>"
-            + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-            + "SELECT DISTINCT ?place ?name"
-            + "WHERE {"
-            + "?place a dbo:Place;"
-            + "a dbo:City;"
-            + "rdfs:label ?name."
-            + "FILTER langMatches(lang(?name),\"en\")"
-            + "FILTER(CONTAINS(?name, \"%1$s\"))"
-            + "}";
-
-    /**
      * @param - The text that will be searched as city and person.
      */
     public  static String semanticBody =
@@ -63,4 +33,70 @@ public final class Queries {
                     + "}"
                     + "}"
                     + "}";
+
+
+    public static String isCity =
+            "PREFIX  dbo: <http://dbpedia.org/ontology/>\n"
+            + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+            + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+            + "SELECT"
+            + "(\"true\" AS ?result)"
+            + "WHERE"
+            + "{"
+	        + "<%1$s> rdf:type ?type."
+	        + "<%1$s> rdfs:label ?label."
+            + "FILTER langMatches( lang(?label), \"EN\" )"
+            + "FILTER (?type IN (dbo:City))"
+            + "}"
+            + "GROUP BY"
+            + "?label";
+
+    public static String getCityProperties =
+            "PREFIX  dbo: <http://dbpedia.org/ontology/>\n"
+            + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+            + "SELECT"
+            + "(?label 	 		        AS ?name)"
+	        + "(SAMPLE(?counrtyLabel)   AS ?counrtyName)"
+            + "(SAMPLE(?peopleCount)    AS ?population)"
+            + "(SAMPLE(?thumbnail)      AS ?image)"
+            + "(SAMPLE(?webSite) 	    AS ?webPage)"
+            + "WHERE"
+            + "{"
+            + "<%1$s> rdfs:label               ?label."
+            + "<%1$s> dbo:country              ?ruler."
+            + "<%1$s> dbo:populationTotal      ?peopleCount."
+            + "<%1$s> dbo:thumbnail            ?thumbnail."
+            + "<%1$s> dbo:wikiPageExternalLink ?webSite."
+            + "?ruler rdfs:label ?counrtyLabel."
+            + "FILTER langMatches( lang(?label), \"EN\" )"
+            + "}"
+            + "GROUP BY "
+            + "?label "
+            + "LIMIT 1";
+
+    public static String getIndividualProperties =
+            "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n"
+            + "PREFIX  dbo: <http://dbpedia.org/ontology/>\n"
+            + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+            + "SELECT "
+            + "?name "
+            + "?jobTitle "
+            + "(SAMPLE(?geburtstag)     AS ?birthDate) "
+            + "(SAMPLE(?homeTownLabel)  AS ?birthPlace) "
+            + "(SAMPLE(?thumbnail)      AS ?image) "
+            + "(SAMPLE(?webSite)        AS ?webPage) "
+            + "WHERE"
+            + "{"
+            + "<%1$s> foaf:name                   ?name."
+            + "<%1$s> dbo:birthDate               ?geburtstag."
+            + "<%1$s> dbo:birthPlace              ?homeTown."
+            + "<%1$s> dbo:occupation              ?occupation."
+            + "<%1$s> dbo:thumbnail               ?thumbnail."
+            + "<%1$s> dbo:wikiPageExternalLink    ?webSite."
+            + "?homeTown rdfs:label ?homeTownLabel."
+            + "?occupation dbo:title ?jobTitle."
+            + "FILTER langMatches( lang(?name), \"EN\" )"
+            + "}"
+            + "GROUP BY "
+            + "?name ?jobTitle";
 }
